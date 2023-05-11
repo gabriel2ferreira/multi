7a09f00af05de1d9670a4268ff7d81cd49256f91
gabriel2ferreira <108493101+gabriel2ferreira@users.noreply.github.com>
2023-05-11 16:13:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -40,3 +40,70 @@ btnDepoimentos.forEach(botao => {
   });
 });
 
+$(document).ready(function() {
+  // Código a ser executado após o carregamento completo da página
+  // Adiciona o evento de clique no botão do menu
+$('.menu-botao').click(function() {
+  // Toggles classe ativo para menu-botao e menu-lista
+  $(this).toggleClass('ativo');
+  $('.menu-lista').toggleClass('ativo');
+});
+
+// Fecha o menu de navegação ao clicar em um link do menu
+$('.menu-lista a').click(function() {
+  $('.menu-botao').removeClass('ativo');
+  $('.menu-lista').removeClass('ativo');
+});
+
+  // Inicializa o slider com o plugin slick
+$('.slider-principal').slick({
+  autoplay: true,
+  arrows: false,
+  dots: true,
+});
+
+// Adiciona o evento de clique no botão de "Próximo" do slider
+$('.slider-proximo').click(function() {
+  $('.slider-principal').slick('slickNext');
+});
+
+// Adiciona o evento de clique no botão de "Anterior" do slider
+$('.slider-anterior').click(function() {
+  $('.slider-principal').slick('slickPrev');
+});
+// Adiciona o evento de clique em um link de âncora com classe "scroll-suave"
+$('a.scroll-suave').click(function() {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    var hash = this.hash;
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function() {
+      window.location.hash = hash;
+    });
+  }
+});
+// Inicializa o carrossel de depoimentos com o plugin slick
+$('.depoimento-carousel').slick({
+  arrows: false,
+  dots: true,
+});
+
+// Adiciona o evento de clique no botão "Ver Todos" dos depoimentos
+$('.depoimento-ver-todos').click(function() {
+  $('.depoimento-modal').addClass('ativo');
+});
+
+// Adiciona o evento de clique no botão "Fechar" do modal dos depoimentos
+$('.depoimento-modal-fechar').click(function() {
+  $('.depoimento-modal').removeClass('ativo');
+});
+// Adiciona o evento de clique no botão "Voltar ao Topo"
+$('.voltar-topo').click(function() {
+  $('html, body').animate({scrollTop : 0},800);
+  return false;
+});
+
+});
+
