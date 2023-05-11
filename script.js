30dbc6f524ad6d0a99c5b994574c97cafc43e9eb
gabriel2ferreira <108493101+gabriel2ferreira@users.noreply.github.com>
2023-05-11 16:57:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,90 +1,66 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-  // variáveis para manipulação dos elementos do DOM
-  const menuMobile = $(".menu-mobile");
-  const navbar = $(".navbar");
-  const navbarLinks = $(".navbar-links");
-  const logo = $(".logo");
-  const topButton = $(".top-button");
+  // Seleciona todos os elementos <a> dentro de <li>
+  var menuItens = $("nav ul li a");
 
-  // função para abrir e fechar o menu mobile
-  menuMobile.click(function() {
-    navbar.toggleClass("navbar-open");
-    navbarLinks.slideToggle(300);
-    logo.toggleClass("logo-menu-open");
+  // Seleciona a barra de navegação
+  var navbar = $("nav");
+
+  // Seleciona o botão de scroll para o topo
+  var scrollTopButton = $("#scroll-top-btn");
+
+  // Função para mudar a classe ativa do item de menu selecionado
+  function toggleActiveClass(selectedMenu) {
+    menuItens.removeClass("active");
+    selectedMenu.addClass("active");
+  }
+
+  // Função para fechar o menu móvel quando um link é clicado
+  function closeMobileMenu() {
+    if (navbar.hasClass("mobile-menu-active")) {
+      navbar.removeClass("mobile-menu-active");
+    }
+  }
+
+  // Função para rolar suavemente para o topo da página
+  function scrollToTop() {
+    $("html, body").animate({
+      scrollTop: 0
+    }, "slow");
+  }
+
+  // Adiciona um evento de clique em cada item do menu
+  menuItens.click(function (event) {
+    event.preventDefault();
+    var selectedMenu = $(this);
+    var target = selectedMenu.attr("href");
+    toggleActiveClass(selectedMenu);
+
+    $("html, body").animate({
+      scrollTop: $(target).offset().top - 100
+    }, "slow");
+
+    closeMobileMenu();
   });
 
-  // função para fechar o menu mobile quando um link for clicado
-  navbarLinks.click(function() {
-    navbar.removeClass("navbar-open");
-    navbarLinks.slideUp(300);
-    logo.removeClass("logo-menu-open");
+  // Adiciona um evento de clique no botão de scroll para o topo
+  scrollTopButton.click(function () {
+    scrollToTop();
   });
 
-  // função para voltar ao topo da página
-  topButton.click(function() {
-    $("html, body").animate({ scrollTop: 0 }, "slow");
-  });
-
-  // função para esconder e mostrar o botão de voltar ao topo
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      topButton.fadeIn();
+  // Adiciona um evento de rolagem para mostrar o botão de scroll para o topo
+  $(window).scroll(function () {
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop > 500) {
+      scrollTopButton.addClass("show-scroll-top");
     } else {
-      topButton.fadeOut();
+      scrollTopButton.removeClass("show-scroll-top");
     }
   });
 
-});
-$(document).ready(function() {
-
-  // variáveis para manipulação dos elementos do DOM
-  const depoimentos = $(".depoimentos");
-  const depoimentoItems = $(".depoimento-item");
-  const depoimentoButtons = $(".depoimento-item .botao");
-  let depoimentoIndex = 0;
-
-  // função para mostrar o próximo depoimento
-  function nextDepoimento() {
-    depoimentoItems.eq(depoimentoIndex).removeClass("ativo");
-    depoimentoIndex = (depoimentoIndex + 1) % depoimentoItems.length;
-    depoimentoItems.eq(depoimentoIndex).addClass("ativo");
-  }
-
-  // função para mostrar o depoimento anterior
-  function prevDepoimento() {
-    depoimentoItems.eq(depoimentoIndex).removeClass("ativo");
-    depoimentoIndex = (depoimentoIndex - 1 + depoimentoItems.length) % depoimentoItems.length;
-    depoimentoItems.eq(depoimentoIndex).addClass("ativo");
-  }
-
-  // função para exibir o botão de próximo depoimento
-  function showNextButton() {
-    depoimentoButtons.eq(1).fadeIn();
-  }
-
-  // função para esconder o botão de próximo depoimento
-  function hideNextButton() {
-    depoimentoButtons.eq(1).fadeOut();
-  }
-
-  // função para exibir o botão de depoimento anterior
-  function showPrevButton() {
-    depoimentoButtons.eq(0).fadeIn();
-  }
+  // Adiciona um evento de clique no ícone do menu móvel para exibir/ocultar o menu
+  $(".menu-toggle").click(function () {
+    navbar.toggleClass("mobile-menu-active");
+  });
 
 });
-$(document).ready(function() {
-
-  // variáveis para manipulação dos elementos do DOM
-  const depoimentos = $(".depoimentos");
-  const depoimentoItems = $(".depoimento-item");
-  const depoimentoButtons = $(".depoimento-item .botao");
-  let depoimentoIndex = 0;
-
-  // função para esconder o botão de depoimento anterior
-  function hidePrevButton() {
-    depoimentoButtons.eq(0).fadeOut();
-  }
-
-  // função para atualizar a exibição dos botões de navegação
